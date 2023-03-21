f74ae50afff7a1d48f0ab86f90c97ff5c27f8f29
Aron Felippe Kerkhoven <43013959+Nirayuki@users.noreply.github.com>
2023-03-21 20:56:03 -0300

    Project Init

---
+++ main.py
@@ -0,0 +1,69 @@
+import pygame 
+from pygame.locals import *
+import os
+
+
+pygame.init()
+
+
+
+# Variaveis ----------------------------------------------------------------
+
+SCREEN_WIDTH, SCREEN_HEIGTH = 1200, 720 
+TILE_SIZE = 200
+
+
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGTH))
+pygame.display.set_caption("Plataform Game")
+
+
+# Colors ----------------------------------------------------------------
+
+GREEN = (124,252,0)
+WHITE = (225, 225, 225)
+
+# Class ----------------------------------------------------------------
+
+class World():
+    def __init__(self, data):
+
+
+        #Load Images
+
+        for row in data:
+            for tile in row:
+                if tile == 1:
+                    block = pygame.Rect(0, 0, TILE_SIZE, TILE_SIZE)
+
+
+# Funções ----------------------------------------------------------------
+
+
+world_data = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+
+run = True
+while run:
+    SCREEN.fill(GREEN)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    pygame.display.update()
+
+
+pygame.quit()
